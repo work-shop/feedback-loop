b739195f3ac56a3d0c5dc3cb21868f749c6cb537
Greg Nemes <mail@gregnemes.com>
2019-02-08 09:44:28 -0500

    minor changes

--- .deploy-production.sh
+++ .deploy-production.sh
@@ -8,14 +8,14 @@ source ./.env
 #scp -P $KINSTA_PORT -r wp-content/uploads $KINSTA_USER@$KINSTA_IP:./public/wp-content/
 
 # Custom Theme
-#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
+scp -P $KINSTA_PRODUCTION_PORT -r wp-content/themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
 
 # Bundles only
 #scp -P $KINSTA_PRODUCTION_PORT -r wp-content/themes/custom/bundles $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom
 
 # Plugins and must use plugins
-scp -P $KINSTA_PRODUCTION_PORT -r wp-content/plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
-scp -P $KINSTA_PRODUCTION_PORT -r wp-content/mu-plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
+#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
+#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/mu-plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
 
 #specific plugins
 #scp -P $KINSTA_PRODUCTION_PORT -r wp-content/plugins/wc-product-customer-list-premium $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/plugins
