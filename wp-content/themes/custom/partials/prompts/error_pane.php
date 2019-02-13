a7c9d458aaa241f919e5e6caf82fbf03a8b7d134
Greg Nemes <mail@gregnemes.com>
2019-02-13 10:21:29 -0500

    completed errors and thank you

--- wp-content/themes/custom/partials/prompts/error_pane.php
+++ wp-content/themes/custom/partials/prompts/error_pane.php
@@ -1,5 +1,14 @@
 <div id="error-pane" class="error-pane off inactive">
-    <div class="error-pane-close-button close-button">x</div>
-    <h3 class="error-header">Whoops!</h3>
-    <p class="error-text"></p>
+	<div class="row">
+		<div class="col-10">
+			<h3 class="error-header">Whoops!</h3>
+			<p class="error-text"></p>
+		</div>
+		<div class="col-2">
+			<div class="error-pane-close-button close-button">
+				<span class="icon" data-icon="ﬂ">
+				</span>
+			</div>
+		</div>
+	</div>
 </div>
--- wp-content/themes/custom/partials/prompts/thankyou_pane.php
+++ wp-content/themes/custom/partials/prompts/thankyou_pane.php
@@ -1,11 +1,15 @@
 <div id="feedback-input-thank-you-pane" class="inactive">
-    <div class="row">
-        <h3 class="brand feedback-input-thank-you-header">
-            <?php the_field('thank_you_overview', 'options'); ?>
-        </h3>
-        <h4 class="brand feedback-input-overview-text">
-            <?php the_field('thank_you_details', 'options'); ?>
-        </h4>
-        <div class="done-button">✔</div>
-    </div>
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="brand feedback-input-thank-you-header">
+				<span class="icon feedback-input-thank-you-header-icon" data-icon="∞"></span> Thank You!
+			</h2>
+			<h3 class="brand feedback-input-thank-you-overview">
+				<?php the_field('thank_you_overview', 'options'); ?>
+			</h3>
+			<h4 class="brand feedback-input-thank-you-text">
+				<?php the_field('thank_you_details', 'options'); ?>
+			</h4>
+		</div>
+	</div>
 </div>
