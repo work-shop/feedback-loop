ad220aafc04d35a24e2ca64727f143759db970c5
Nic Schumann <nicolas_schumann@brown.edu>
2019-03-04 09:39:56 -0500

    [gtag] removes localhost gaurd from gtag include.

--- wp-content/themes/custom/partials/footer.php
+++ wp-content/themes/custom/partials/footer.php
@@ -2,23 +2,21 @@
 
         <?php //get_template_part('partials/footer_site' ); ?>
 
-        <?php get_template_part('partials/modals' ); ?>
+        <?php get_template_part('partials/modals');?>
 
         <?php //get_template_part('partials/viewport_label' ); ?>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134514873-1"></script>
-        <script>
-          if ( window.location.hostname !== 'localhost' ) {
+        <script>              
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
               gtag('config', 'UA-134514873-1');
-          }
         </script>
 
-        <?php wp_footer(); ?>
+        <?php wp_footer();?>
 
     </body>
 </html>
